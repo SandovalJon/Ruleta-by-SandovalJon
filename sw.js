c476934bcdfdcb5f11055a5c457c5eee0e11c72c
SandovalJon <sandovaljon@users.noreply.github.com>
2026-09-23 22:40:29 -0600

    Reiniciar repo: archivos esenciales

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'ruleta-v2';
+const urlsToCache = [
+    '/',
+    '/Ruleta-by-SandovalJon/',
+    '/Ruleta-by-SandovalJon/index.html',
+    '/Ruleta-by-SandovalJon/manifest.json',
+    '/Ruleta-by-SandovalJon/icon-192.png',
+    '/Ruleta-by-SandovalJon/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then((response) => {
+                    if (!response || response.status !== 200) {
+                        return response;
+                    }
+                    const responseToCache = response.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseToCache);
+                    });
+                    return response;
+                }).catch(() => {
+                    if (event.request.destination === 'document') {
+                        return caches.match('/Ruleta-by-SandovalJon/index.html');
+                    }
+                });
+            })
+    );
+});
